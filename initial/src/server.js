4e4821182dda2eecf80aa7116f4d20e8d96c4004
Madhu Ranjan <madhu.ranjan@softrams.com>
2025-04-18 09:39:40 -0400

    Initial project setup with HTML structure, CSS styling, JavaScript functionality, and server configuration.

---
+++ initial/src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { resolve }  from 'path';
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(('./src/public/')));
+
+app.get('/', (req, res) => {
+    res.sendFile(resolve(__dirname, './public/index.html'));
+});
+
+app.get('/api', (req, res) => {
+    res.send('Welcome to AI Workshop');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
